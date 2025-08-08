7015c4de9d3ae98d3a7f1a64830c0c91f22a7d06
mohamed magdy <mohamedmagdy31098@gmail.com>
2025-08-08 17:49:34 +0300

    [1] [Time Beats: 98.88%] [Memory Beats: 43.76%] - LeetPush

---
+++ Two-Sum.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> numToIndex = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            int complement = target - nums[i];
+
+            if (numToIndex.ContainsKey(complement)) {
+                return new int[] { numToIndex[complement], i };
+            }
+
+            numToIndex[nums[i]] = i;
+        }
+
+        return Array.Empty<int>(); // should never happen
+    }
+}
